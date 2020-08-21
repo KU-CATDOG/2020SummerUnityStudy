ca92a0ef00448eff7b3a033f75247c5275fb411e
RyuJiSeok <roadmageb@naver.com>
2020-08-21 23:45:16 +0900

    총알 클릭 시 발사

---
+++ PracticeUnityProject/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public float speed = 20f;
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
--- PracticeUnityProject/Assets/Scripts/PlayerController.cs
+++ PracticeUnityProject/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    public Camera camera;
+    public Camera cam;
     public float speed = 2f;
+
+    public Transform bulletOut;
+    public GameObject bulletPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +20,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Rotate();
+        Fire();
+    }
+
+    void Move()
+    {
         float dx = Input.GetAxis("Horizontal");
         float dz = Input.GetAxis("Vertical");
         transform.position += new Vector3(dx * speed * Time.deltaTime, 0, dz * speed * Time.deltaTime);
 
-        RaycastHit hit;
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+    }
 
-        if(Physics.Raycast(ray,out hit,LayerMask.GetMask("Floor")))
+    void Rotate()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, LayerMask.GetMask("Floor")))
         {
             Vector3 lookPoint = new Vector3(hit.point.x, transform.position.y, hit.point.z);
             transform.LookAt(lookPoint, Vector3.up);
+        }
+    }
+
+    void Fire()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletPrefab, bulletOut.position, bulletOut.rotation);
         }
     }
 }
