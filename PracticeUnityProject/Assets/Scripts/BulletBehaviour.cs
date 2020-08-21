1d2c135638f8115436f0891dea7da4b47ae00a37
RyuJiSeok <roadmageb@naver.com>
2020-08-21 23:47:52 +0900

    총알 수명 시간 제한 추가

--- PracticeUnityProject/Assets/Scripts/BulletBehaviour.cs
+++ PracticeUnityProject/Assets/Scripts/BulletBehaviour.cs
@@ -6,8 +6,20 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour
 {
     public float speed = 20f;
+    public float bulletLifeTime = 5f;
     void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void Start()
+    {
+        StartCoroutine(BulletLifeOver());
+    }
+
+    IEnumerator BulletLifeOver()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
     }
 }
