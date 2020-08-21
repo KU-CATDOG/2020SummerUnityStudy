e053f5bbff861e47841a0278cdcea411da5fa98e
RyuJiSeok <roadmageb@naver.com>
2020-08-21 22:35:20 +0900

    플레이어 움직임, 카메라 위치 설정

---
+++ PracticeUnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dx = Input.GetAxis("Horizontal");
+        float dz = Input.GetAxis("Vertical");
+        transform.position += new Vector3(dx * speed * Time.deltaTime, 0, dz * speed * Time.deltaTime);
+    }
+}
