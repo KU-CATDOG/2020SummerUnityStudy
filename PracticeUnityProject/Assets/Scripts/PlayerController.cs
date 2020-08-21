0e72631984475e65f1d1df978703b975a1857828
RyuJiSeok <roadmageb@naver.com>
2020-08-21 23:26:20 +0900

    총 추가, 커서 쪽을 바라봄

--- PracticeUnityProject/Assets/Scripts/PlayerController.cs
+++ PracticeUnityProject/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera camera;
     public float speed = 2f;
     // Start is called before the first frame update
     void Start()
@@ -17,5 +18,14 @@ public class PlayerController : MonoBehaviour
         float dx = Input.GetAxis("Horizontal");
         float dz = Input.GetAxis("Vertical");
         transform.position += new Vector3(dx * speed * Time.deltaTime, 0, dz * speed * Time.deltaTime);
+
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray,out hit,LayerMask.GetMask("Floor")))
+        {
+            Vector3 lookPoint = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            transform.LookAt(lookPoint, Vector3.up);
+        }
     }
 }
